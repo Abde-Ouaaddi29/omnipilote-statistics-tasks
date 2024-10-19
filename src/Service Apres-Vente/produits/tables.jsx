77e081951cc0f9dafeae28afd5256971fe8847b8
mohamed-dabach <2000123100328@ofppt-edu.ma>
2024-10-20 00:20:29 +0100

    add table 2

--- src/Service Apres-Vente/produits/tables.jsx
+++ src/Service Apres-Vente/produits/tables.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { technicienStats, tickets } from "../../API/data";
 
 export default function Tables() {
   const clients = useSelector((state) => state.tickets.clients);
-//   const tickets = useSelector((state) => state.tickets.tickets);
+  //   const tickets = useSelector((state) => state.tickets.tickets);
   console.log(clients);
 
   const TablePerClient = () => {
-
     // const openTicket = (id) => {
     //   if()
     // };
@@ -44,7 +44,10 @@ export default function Tables() {
   return (
     <div className="p-4 bg-gray-100">
       <div>table 1</div>
-      <div>table 2</div>
+      <div className="grid lg:grid-cols-2  md:lg:grid-cols-2 grid-cols-1 gap-3">
+        <TechnicienDataTabe isTecheFerme={true} />
+        <TechnicienDataTabe isTecheFerme={false} />
+      </div>
       <div>table 3</div>
       <div className="grid lg:grid-cols-3 xl:lg:grid-cols-3 md:lg:grid-cols-2 grid-cols-1 gap-3">
         <TablePerClient />
@@ -52,5 +55,63 @@ export default function Tables() {
         <TablePerClient />
       </div>
     </div>
+  );
+}
+
+// hi abdo this what here my modifications
+
+// if isTeckeFerme show "Nombre de Tickets fermés par technicien" else show "Durée moyenne d’interventions par technicien"
+
+function TechnicienDataTabe({ isTeckeFerme = false }) {
+  return (
+    <>
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 border ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+            <tr>
+              <th className="text-center px-6 py-4" colSpan={3}>
+                Nombre de Tickets fermés par technicien
+              </th>
+            </tr>
+            <tr>
+              <th scope="col" className="px-6 py-3 border">
+                Technicien
+              </th>
+              <th scope="col" className="px-6 py-3 border">
+                Ce mois
+              </th>
+              <th scope="col" className="px-6 py-3 border">
+                Cette année
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {technicienStats &&
+              technicienStats?.map((technicien) => {
+                return (
+                  <tr className="odd:bg-white border" key={technicien.name}>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {technicien.name}
+                    </th>
+                    <td className="px-6 py-4 border">
+                      {isTeckeFerme
+                        ? technicien.ticketsFeremeCeMois
+                        : technicien.moyenneInterventionsCeMois}
+                    </td>
+                    <td className="px-6 py-4 border">
+                      {isTeckeFerme
+                        ? technicien.ticketsFeremeCeMois
+                        : technicien.moyenneInterventionsCetteAnnee}
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
